14be180c7e093006e3ea6c852f27ef238d96c99f
Germán Heim <germanheim@hotmail.com>
2026-07-22 20:33:42 -0300

    Make gurobi test compile in non-unix

--- crates/gurobi-rs/tests/compile-tests/utils.rs
+++ crates/gurobi-rs/tests/compile-tests/utils.rs
@@ -1,7 +1,11 @@
 #[macro_export]
 macro_rules! create_model {
   ($gag:ident, $model:ident $(,$var:ident)*) => {
+    // `gag` suppresses the Gurobi license banner on stdout, but only builds on Unix.
+    #[cfg(unix)]
     let $gag = gag::Gag::stdout().unwrap();
+    #[cfg(not(unix))]
+    let $gag = ();
     let mut $model = Model::new("test")?;
     $(
       let $var = add_binvar!($model)?;
